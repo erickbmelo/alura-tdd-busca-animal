d2f2087a56a713558d1f1e6d1313fd314e219ffd
erickbmelo <erick.melo@sorocaba.sp.gov.br>
2023-02-27 22:56:35 -0300

    URL do servidor e teste falha

--- setup/tests.py
+++ setup/tests.py
@@ -5,4 +5,14 @@ from selenium import webdriver
 class AnimaisTestCase(LiveServerTestCase):
     
     def setUp(self):
-        self.browser = webdriver.Edge('C:\Users\Erick\Documents\ALURA-TDD\msedgedriver.exe')
+        self.browser = webdriver.Edge(r'C:\Users\Erick\Documents\ALURA-TDD\msedgedriver.exe')
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_abre_janela_do_edge(self):
+        self.browser.get(self.live_server_url)
+
+    def test_deu_ruim(self):
+        """teste de exemplo de erro"""
+        self.fail('Teste falhou - deu ruim mesmo')
